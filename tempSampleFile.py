5f494eead286946e8145b3089dee160082e72914
shouvikcirca <goldenfibo1123@gmail.com>
2022-11-15 12:39:08 +0530

    Created workflow for face detection in video

--- tempSampleFile.py
+++ tempSampleFile.py
@@ -8,60 +8,46 @@ from deepface import DeepFace
 import os
 import cv2
 
-app = FaceAnalysis(providers=['CPUExecutionProvider'])
-app.prepare(ctx_id=0, det_size=(640, 640))
-img = ins_get_image('facesImage')
-faces = app.get(img)
-
-for i in range(len(faces)):
-            xmin, ymin, xmax, ymax = faces[i].bbox
-            croppedIm = Image.fromarray(img).crop((xmin, ymin, xmax, ymax))
-            croppedIm.save('{}_face.jpg'.format(i))
-
-
-
-victorDistance = float('inf')
-victorFace = ''
-for i in range(len(faces)):
-    imageName = '{}_face.jpg'.format(i)
-    result = DeepFace.verify(img1_path = "facesImage4.jpg", img2_path = imageName, enforce_detection=False)
-    print('{}: {}'.format(i, result['distance']))
-    if result['distance'] < victorDistance:
-        victorDistance = result['distance']
-        victorFace = i
-
-
-
-# To draw box on the face with the smallest cosine distance
-victorFace = [faces[victorFace]]
-rimg = app.draw_on(img, victorFace)
-cv2.imwrite("./t1_output.jpg", rimg)
-
-
-
-
-# Removing the generated clipped versions
-for i in range(len(faces)):
-    imageName = '{}_face.jpg'.format(i)
-    os.remove(imageName)
-
-
-videoTitle = ''
+videoTitle = 'facedetrecvideo.mp4'
 cap = cv2.VideoCapture(videoTitle)
-
 
 if not cap.isOpened():
 	cap = cv2.VideoCapture(0)
 if not cap.isOpened():
 	raise IOError("Cannot open video")
 
-
+app = FaceAnalysis(providers=['CPUExecutionProvider'])
+app.prepare(ctx_id=0, det_size=(640, 640))
 
 while True:
 	ret, frame = cap.read()
-	ClassIndex, confidence, bbox = 
+	faces = app.get(frame)
+	victorDistance = float('inf')
+	victorFace = ''
+	for i in range(len(faces)):
+		xmin, ymin, xmax, ymax = faces[i].bbox
+		croppedIm = Image.fromarray(frame).crop((xmin, ymin, xmax, ymax))
+		isExists = os.path.exists('face.jpg')
+		if isExists:
+			os.remove('face.jpg')
+		croppedIm.save('face.jpg')
+		result = DeepFace.verify(img1_path = "face.jpg", img2_path = 'facedetrecsubject.png', enforce_detection=False)
+		cosineDistance = result['distance']
+		if cosineDistance < victorDistance:
+			victorDistance = cosineDistance
+			victorFace = i
+	xmin, ymin, xmax, ymax = faces[victorFace].bbox
+	cv2.rectangle(frame,(int(xmin), int(ymin)),(int(xmax), int(ymax)),(255,0,0),2)
+	cv2.imshow('',frame)
+
+	if cv2.waitKey(2) & 0xFF == ord('q'):
+		break
 
 
+cap.release()
+cv2.destroyAllWindows()
 
 
-
+#ClassIndex, confidence, bbox = model.detect(frame, confThreshold=0.55)
+#xmin, ymin, xmax, ymax = bbox
+#croppedIm = Image.fromarray(frame).crop((xmin, ymin, xmax, ymax))
